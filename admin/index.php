64f82177b5a2c55d83510c1d73e728418aef6139
Anirudh33Iyengar <anirudh3iyengar@gmail.com>
2017-06-07 14:33:18 +0530

    ppt

--- admin/index.php
+++ admin/index.php
@@ -23,7 +23,7 @@
         </ul>
     </div>
 	<div class="table">
-       <table style="width:100%">
+       <table style="width:100%" border="1" >
 			<tr>
 				<th>Name</th>
 				<th>USN</th>
@@ -37,13 +37,14 @@
 				<th><?php echo $row['usn'];?></th>
 				<th><?php echo $row['certificateName'];?></th>
 				<th><input type="submit" name="view" value="View" onclick="mover()"/><th>
-                <th><input type="hidden" value="<?php echo $row['id']?>" name="idret" </th>
+                <th><input type="hidden" value="<?php echo $row['id']?>" name="idret" ></th>
 				</tr>
 			<tr>
 			</form>				
 			<?php endwhile; ?>		
-			
-    </div>
-    
+		   </tr>	
+		</table>
+		   
+		</div>
     </body>
-</html>3
+</html>
