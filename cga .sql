a505d5d8f04f619ffde0c898ea99aea735a7cdab
akshatha <akshatha.s2496@gmail.com>
2017-06-08 13:27:30 +0530

    changes

--- cga .sql
+++ cga .sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Jun 07, 2017 at 07:29 PM
+-- Generation Time: Jun 08, 2017 at 09:55 AM
 -- Server version: 10.1.21-MariaDB
 -- PHP Version: 5.6.30
 
