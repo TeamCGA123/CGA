4d623b155fc76f81ddb81a18c619add776779596
Anirudh33Iyengar <anirudh3iyengar@gmail.com>
2017-06-08 09:17:06 +0530

    latest ppt

--- admin/view.php
+++ admin/view.php
@@ -6,32 +6,35 @@ if( isset($_POST['view']))
 	$q= $_POST['idret'];
 	$w="SELECT * FROM requests WHERE id=$q";
 	$details=mysqli_query($con,$w);
-
-
 }
-
 ?>
 <!Doctype html>
 <html>
 <head>
 </head>
 <body>
-<pre>
 
-<h1>Details of student</h1>
+
+<h1 align=center>Details of student</h1>
 <?php
 while($row = $details -> fetch_assoc()){
 	echo "<html><body>";
-echo "<b><p>USN:</b>";
+echo "<b><p>NAME:</b>";
+echo $row['name'];
+ echo "<br><b>USN:</b>";
 echo $row['usn'];
- echo "<br><b>NAME:</b>";
-echo $row['name']."</p>";
+echo "<br><b>Father's Name:</b>";
+echo $row['fathersname'];
+echo "<br/><b>MOBILE NO:</b>";
+echo $row['mobileno'];
+ echo "<br><b>EMAIL:</b>";
+echo $row['email'];
+echo "</p>";
 echo "</body></html>";
 }
-
-
 ?>
+<input type="submit" value="Process"/>&nbsp;&nbsp;
+<input type="submit" value="Ignore"/>
 
-</pre>
 </body>
 </html>
