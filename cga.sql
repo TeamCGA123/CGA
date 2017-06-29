25beb8180f9a1a335145c99ee6b5f1e06ec8c3fa
akshatha <akshatha.s2496@gmail.com>
2017-06-29 17:52:49 +0530

    db

--- cga.sql
+++ cga.sql
@@ -3,12 +3,12 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Jun 08, 2017 at 10:01 AM
+-- Generation Time: Jun 29, 2017 at 02:21 PM
 -- Server version: 10.1.21-MariaDB
 -- PHP Version: 5.6.30
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
-SET time_zone = "+5:30";
+SET time_zone = "+00:00";
 
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
